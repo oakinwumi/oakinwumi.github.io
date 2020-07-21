246a55942dde1a99c7759d0d4895f44b986d6ce6
Dabira12 <dabira.akinwumi@gmail.com>
2020-07-21 11:21:55 -0400

    First module 9 commit

--- Module 9/myapp/src/App.js
+++ Module 9/myapp/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+import {BrowserRouter,Route, Switch} from 'react-router-dom'
+import About from './components/About'
+import Rental from './components/Rental'
+import Faq from './components/Faq'
+import carInfo from './components/carInfo'
+import confirmation from './components/confirmation'
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>My app</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={About}/>
+              <Route path='/rental' component={Rental}/>
+              <Route path='/Faq' component={Faq}/>
+              <Route path='/confirmation' component={confirmation}/>
+              <Route path='/cars/:car_id' component={carInfo}/>
+              
+            </Switch>
+          
+
+        </div>
+      </BrowserRouter>
+      
+    );
+  }
+  
 }
 
 export default App;
